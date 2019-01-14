ab858df086e0a89ae16f29dc3da679f4aaccc621
Nick Romans <nicholas.romans@gmail.com>
2019-01-14 20:28:48 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     self.characters.collect do |character|
-      "#{character.name} - #{chracter.show}"
+      "#{character.name} - #{chracter.show.name}"
     end
   end
 
